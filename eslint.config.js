c26e2d6e90c096ed32a4d8b07c3f5c62718fd301
stefand799 <dinustefan1996@yahoo.com>
2026-04-26 10:17:22 +0300

    fix: migrate ESLint config to flat config format (v10)

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,7 @@ module.exports = [
         rules: {
             ...tseslint.configs.recommended.rules,
             "@typescript-eslint/no-explicit-any": "warn",
-            "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+            "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
             "no-console": "off",
         },
     },
